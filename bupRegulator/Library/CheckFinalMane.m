c33d450a6d6faded7853c0411292c7353955fc42
Андрей Селиверстов <7a653s9@mail.ru>
2019-11-27 10:51:44 +0300

    1) CheckFinalMane: * Угол до TDP вычислялся неверно, исправлено 2) Модель: * Завел поправку на TDP и обратную связь от БИМов; * Попытка организовать алгоритм ступенчатой потери высоты (пока привязан на  OnlyTDP), надо вынести в отдельный режим; 3) Библиотека slx: * Блоки отсортированы и добавлены краткие описания; 4) Tuner: * Закоментировал обрезку результатов моделирования по высоте 300м, добавил вместо этого блоки STOP в модели.

--- bupRegulator/Library/CheckFinalMane.m
+++ bupRegulator/Library/CheckFinalMane.m
@@ -13,16 +13,22 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function status = CheckFinalMane ()
     %% Вычислим расстояние с помощью функции DistanceCalculator()
-    Distance = DistanceCalculator(ActualPoint, TDPoint);
+    Distance = DistanceCalculator(ActualPoint, TDPoint); %+TDPPopr
     %% Вычисляем время необходимое для преодоления расстояния до TDP и до посадки
-    T_hor = Distance/V_hor;
-    T_ver = (ActualPoint(3) - (TDPoint(3) + double(ReliefOnTDP)))/V_ver;
+    T_hor = Distance/HorizontalVelocity;
+    T_ver = (ActualPoint(3) - (TDPoint(3) + double(ReliefOnTDP)))/VerticalVelocity;
     %% Определяем поправку по времени необходимому на разворот
+    % Определим курс до точки приземления
+    Course2TDP = Heading(TDPoint, ActualPoint, 0);
     % Угол разворота
-    angle = abs(Course2TDP-Course) * 180 / pi;
-    if angle > 180
-        angle = angle - 180;
+    angle = Course2TDP - Course;
+    if angle > pi
+        angle = angle - 2*pi; 
     end
+    if angle < (-pi)
+        angle = angle + 2*pi; 
+    end
+    angle = abs(angle * 180 / pi);
 
     % Вычисляем поправку как линейную функцию: 
     T_hor_popr = angle / TurnSpeed;
@@ -37,4 +43,3 @@ function status = CheckFinalMane ()
     else
         status = 0;
     end
-end
